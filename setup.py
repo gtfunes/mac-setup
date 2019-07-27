c1763744ecce9d34d93823ae59d8da4cac4da00c
Gaston Funes <gtfunes@gmail.com>
2019-07-27 15:00:29 -0300

    Install some more useful apps & better messages

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ email = ''
 
 # Check for Xcode Command Line Tools
 if os.system('xcode-select -p') != 0:
-  print "Installing XCode Tools"
+  print "Installing XCode Command Line Tools..."
   os.system('xcode-select --install')
   print "**************************************************************"
-  print "Install the XCode Command Line Tools and run this script again"
+  print "  Install XCode Command Line Tools and run this script again  "
   print "**************************************************************"
   exit()
 
@@ -22,19 +22,20 @@ if os.system('xcode-select -p') != 0:
 while name == '':
   name = raw_input("What's your name?\n").strip()
 
+# Email
 while email == '' or '@' not in email:
   email = raw_input("What's your email?\n").strip()
 
 def show_notification(text):
   os.system('osascript -e \'display notification "'+ text +'" with title "Mac Setup"\' > /dev/null')
 
-print "Hey %s!" % name
+print "Hey %s, lets setup your new Mac!" % name
 print "You'll be asked for your password a few times during this process"
 print "*************************************"
 
 # Create a Private Key
 if not os.path.isfile(os.path.expanduser("~") + '/.ssh/id_rsa.pub'):
-  print "Creating your Private Key"
+  print "Creating your Private Key..."
   os.system('ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "" -C "%s"' % email)
 
 # Set computer name & git info
@@ -46,7 +47,7 @@ os.system('git config --global user.name "%s"' % name)
 os.system('git config --global user.email "%s"' % email)
 
 # Install Brew & Brew Cask
-print "Installing Brew & Brew Cask"
+print "Installing Brew & Brew Cask..."
 os.system('touch ~/.bash_profile')
 os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
 os.system('brew tap caskroom/cask')
@@ -56,19 +57,16 @@ os.system('brew tap caskroom/fonts')
 os.system('brew update && brew upgrade && brew cleanup && brew cask cleanup')
 
 # Install Languages
-print "Installing Git+NodeJS+Python+Ruby"
+print "Installing Git+NodeJS+Python+Ruby..."
 os.system('brew install git node python python3 ruby')
 os.system('brew link --overwrite git node python python3 ruby')
 os.system('brew unlink python && brew link --overwrite python')
 os.system('brew install git-flow git-lfs')
 os.system('git lfs install')
 
-print "Installing Useful Stuff"
+print "Installing Useful Stuff..."
 os.system('brew install graphicsmagick curl wget sqlite libpng libxml2 openssl')
 os.system('brew install bat tldr tree')
-
-print "Installing Command Line Tools"
-os.system('npm install -g yo gulp-cli node-gyp serve ndb')
 
 # OSX Tweaks & Essentials
 print "Installing Quicklook Helpers"
@@ -78,33 +76,33 @@ print "Installing Fonts"
 os.system('brew cask install font-dosis font-droid-sans-mono-for-powerline font-open-sans font-open-sans-condensed font-roboto font-roboto-mono font-roboto-condensed font-roboto-slab font-consolas-for-powerline font-dejavu-sans font-dejavu-sans-mono-for-powerline font-inconsolata font-inconsolata-for-powerline font-lato font-menlo-for-powerline font-meslo-lg font-meslo-for-powerline font-noto-sans font-noto-serif font-source-sans-pro font-source-serif-pro font-ubuntu font-pt-mono font-pt-sans font-pt-serif font-fira-mono font-fira-mono-for-powerline font-fira-code font-fira-sans font-source-code-pro')
 
 print "Installing Essential Apps"
-os.system('brew cask install iterm2 spectacle the-unarchiver')
-os.system('brew cask install google-chrome sublime-merge visual-studio-code spotify slack vlc zoom')
+os.system('brew cask install iterm2 istat-menus spectacle the-unarchiver')
+os.system('brew cask install google-chrome github sublime-merge visual-studio-code spotify franz vlc zoom')
 
 os.system('brew cask fetch qlimagesize')
-show_notification("We need your password")
+show_notification("We need your password:")
 os.system('brew cask install qlimagesize')
 
 # Software
 os.system('brew cask install docker sequel-pro cyberduck insomnia')
 os.system('brew cask fetch java')
 
-show_notification("We need your password")
+show_notification("We need your password:")
 os.system('brew cask install java')
 
 os.system('brew cask install android-studio')
 os.system('brew install android-platform-tools')
 
-show_notification("We need your password")
+show_notification("We need your password:")
 os.system('sudo gem install cocoapods')
 
-show_notification("We need your password")
+show_notification("We need your password:")
 os.system('sudo gem install fastlane --verbose')
 
 os.system('brew cask install imageoptim')
 
 # Oh-My-ZSH
-print "Installing Oh-My-Zsh with Dracula Theme"
+print "Installing Oh-My-Zsh with Dracula Theme..."
 show_notification("We need your password")
 
 # Adapted from https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
@@ -138,11 +136,11 @@ if (subprocess.call(['bash', '-c', 'diff <(tail -n +6 ~/.zshrc) <(tail -n +6  ~/
 # Remove the 'last login' message
 os.system('touch ~/.hushlogin')
 
-print "Dracula Theme will be downloaded to Desktop, select it later from iTerm profile settings"
+print "Dracula Theme will be downloaded to your Desktop, set it later from iTerm profile color settings!"
 os.system('git clone https://github.com/dracula/iterm.git ~/Desktop/dracula-theme/')
 
 # Random OSX Settings
-print "Tweaking OSX Settings"
+print "Tweaking OSX Settings..."
 
 # Finder: show hidden files by default
 os.system('defaults write com.apple.finder AppleShowAllFiles -bool true')
@@ -167,7 +165,7 @@ os.system('defaults write com.apple.dock mru-spaces -bool false')
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 os.system('defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true')
 
-print "Tweaking System Animations"
+print "Tweaking System Animations..."
 os.system('defaults write NSGlobalDomain NSWindowResizeTime -float 0.1')
 os.system('defaults write com.apple.dock expose-animation-duration -float 0.15')
 os.system('defaults write com.apple.dock autohide-delay -float 0')
@@ -175,7 +173,7 @@ os.system('defaults write com.apple.dock autohide-time-modifier -float 0.3')
 os.system('defaults write NSGlobalDomain com.apple.springing.delay -float 0.5')
 os.system('killall Dock')
 
-print "Enabling Automatic Brew Updates & Upgrades"
+print "Enabling Automatic Brew Updates & Upgrades..."
 os.system('brew tap domt4/autoupdate')
 os.system('brew autoupdate --start --upgrade')
 
@@ -195,5 +193,5 @@ os.system('sudo nvram SystemAudioVolume=%00')
 print "*************************************"
 show_notification("All done!")
 
-# Change the shell
+# Change the shell to zsh
 os.system('chsh -s /bin/zsh &> /dev/null')
